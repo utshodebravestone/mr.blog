9020e554c709086656eecc4697cece4aa6c9b8ed
utsho <utshodebravestone@gmail.com>
2023-11-16 16:42:48 +0600

    better transition of nav-items

--- client/src/components/NavItem.jsx
+++ client/src/components/NavItem.jsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 
 const NavItem = ({ text, href }) => {
   return (
-    <li className="group">
+    <li className="relative group">
       <Link to={href} className="mr-2">
         {text}
       </Link>
-      <span className="text-blue-500 font-bold transition-all duration-500 opacity-0 group-hover:opacity-100">
+      <span className="absolute top-0 left-0 text-blue-500 font-bold transition-all duration-500 opacity-0 group-hover:left-[95%] group-hover:opacity-100">
         /
       </span>
     </li>
